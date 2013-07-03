a9dfa0db197165f47af678430455298d0dbf6bb3
Radu Murzea <radu.murzea@gmail.com>
2013-07-03 23:54:30 +0300

    - Added whitespace to XML export functionality depending on the nested level (kind of like a pretty printing)

--- org/javafling/pokerenlighter/simulation/SimulationExport.java
+++ org/javafling/pokerenlighter/simulation/SimulationExport.java
@@ -13,6 +13,8 @@ public class SimulationExport
 {
 	private static final String LINE_END = System.lineSeparator ();
 	
+	private static final String TAB_CHAR = Character.toString ('\t');
+	
 	/**
 	 * Constructs a XML representation of the simulation result and returns it in the form of
 	 * a String.
@@ -35,30 +37,38 @@ public class SimulationExport
 		xml.append (LINE_END);
 		xml.append ("<simulation>");
 		xml.append (LINE_END);
+		xml.append (getNestingCharacters (1));
 		xml.append ("<meta>");
 		xml.append (LINE_END);
+		xml.append (getNestingCharacters (2));
 		xml.append ("<playercount>");
 		xml.append (result.getNrOfPlayers ());
 		xml.append ("</playercount>");
 		xml.append (LINE_END);
+		xml.append (getNestingCharacters (2));
 		xml.append ("<rounds>");
 		xml.append (result.getRounds ());
 		xml.append ("</rounds>");
 		xml.append (LINE_END);
+		xml.append (getNestingCharacters (2));
 		xml.append ("<type>");
 		xml.append (result.getPokerType ());
 		xml.append ("</type>");
 		xml.append (LINE_END);
+		xml.append (getNestingCharacters (1));
 		xml.append ("</meta>");
 		xml.append (LINE_END);
+		xml.append (getNestingCharacters (1));
 		xml.append ("<players>");
 		xml.append (LINE_END);
 		for (int i = 1; i <= result.getNrOfPlayers (); i++)
 		{
+			xml.append (getNestingCharacters (2));
 			xml.append ("<player id=\"");
 			xml.append (i);
 			xml.append ("\">");
 			xml.append (LINE_END);
+			xml.append (getNestingCharacters (3));
 			xml.append ("<handtype>");
 			PlayerProfile profile = result.getPlayer (i - 1);
 			if (profile.getHandType () == HandType.RANDOM)
@@ -75,6 +85,7 @@ public class SimulationExport
 				Card[] cards = profile.getCards ();
 				for (Card card : cards)
 				{
+					xml.append (getNestingCharacters (3));
 					xml.append ("<card>");
 					xml.append (card.toString ());
 					xml.append ("</card>");
@@ -93,6 +104,7 @@ public class SimulationExport
 					{
 						if (range.getValue (row, col))
 						{
+							xml.append (getNestingCharacters (3));
 							xml.append ("<cardtype>");
 							xml.append (Range.rangeNames[row][col]);
 							xml.append ("</cardtype>");
@@ -102,10 +114,12 @@ public class SimulationExport
 				}
 			}
 			
+			xml.append (getNestingCharacters (2));
 			xml.append ("</player>");
 			xml.append (LINE_END);
 		}
 		
+		xml.append (getNestingCharacters (1));
 		xml.append ("</players>");
 		xml.append (LINE_END);
 		
@@ -115,19 +129,23 @@ public class SimulationExport
 		
 		if (flop != null || turn != null || river != null)
 		{
+			xml.append (getNestingCharacters (1));
 			xml.append ("<community>");
 			xml.append (LINE_END);
 			
 			if (flop != null)
 			{
+				xml.append (getNestingCharacters (2));
 				xml.append ("<flop id=\"1\">");
 				xml.append (flop[0].toString ());
 				xml.append ("</flop>");
 				xml.append (LINE_END);
+				xml.append (getNestingCharacters (2));
 				xml.append ("<flop id=\"2\">");
 				xml.append (flop[1].toString ());
 				xml.append ("</flop>");
 				xml.append (LINE_END);
+				xml.append (getNestingCharacters (2));
 				xml.append ("<flop id=\"3\">");
 				xml.append (flop[2].toString ());
 				xml.append ("</flop>");
@@ -136,6 +154,7 @@ public class SimulationExport
 			
 			if (turn != null)
 			{
+				xml.append (getNestingCharacters (2));
 				xml.append ("<turn>");
 				xml.append (turn.toString ());
 				xml.append ("</turn>");
@@ -144,22 +163,27 @@ public class SimulationExport
 			
 			if (river != null)
 			{
+				xml.append (getNestingCharacters (2));
 				xml.append ("<river>");
 				xml.append (river.toString ());
 				xml.append ("</river>");
 				xml.append (LINE_END);
 			}
 			
+			xml.append (getNestingCharacters (1));
 			xml.append ("</community>");
 			xml.append (LINE_END);
 		}
 		
+		xml.append (getNestingCharacters (1));
 		xml.append ("<result>");
 		xml.append (LINE_END);
+		xml.append (getNestingCharacters (2));
 		xml.append ("<threads>");
 		xml.append (result.getNrOfThreads ());
 		xml.append ("</threads>");
 		xml.append (LINE_END);
+		xml.append (getNestingCharacters (2));
 		xml.append ("<duration>");
 		xml.append (result.getDuration ());
 		xml.append ("</duration>");
@@ -167,30 +191,47 @@ public class SimulationExport
 		
 		for (int i = 1; i <= result.getNrOfPlayers (); i++)
 		{
+			xml.append (getNestingCharacters (2));
 			xml.append ("<player id=\"");
 			xml.append (i);
 			xml.append ("\">");
 			xml.append (LINE_END);
+			xml.append (getNestingCharacters (3));
 			xml.append ("<wins>");
 			xml.append (result.getFormattedWinPercentage (i - 1));
 			xml.append ("</wins>");
 			xml.append (LINE_END);
+			xml.append (getNestingCharacters (3));
 			xml.append ("<loses>");
 			xml.append (result.getFormattedLosePercentage (i - 1));
 			xml.append ("</loses>");
 			xml.append (LINE_END);
+			xml.append (getNestingCharacters (3));
 			xml.append ("<ties>");
 			xml.append (result.getFormattedTiePercentage (i - 1));
 			xml.append ("</ties>");
 			xml.append (LINE_END);
+			xml.append (getNestingCharacters (2));
 			xml.append ("</player>");
+			xml.append (LINE_END);
 		}
-		
-		xml.append (LINE_END);
+
+		xml.append (getNestingCharacters (1));
 		xml.append ("</result>");
 		xml.append (LINE_END);
 		xml.append ("</simulation>");
 		
 		return xml.toString ();
+	}
+	
+	private static String getNestingCharacters (int nestedLevel)
+	{
+		StringBuilder sb = new StringBuilder ();
+		for (int i = 0; i < nestedLevel; i++)
+		{
+			sb.append (TAB_CHAR);
+		}
+		
+		return sb.toString ();
 	}
 }
