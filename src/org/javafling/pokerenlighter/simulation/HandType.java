16091acadca575fea09de33972b4346f18fa4412
Radu Murzea <radu.murzea@gmail.com>
2013-07-18 20:38:46 +0300

    Moved repository root one folder up

---
+++ src/org/javafling/pokerenlighter/simulation/HandType.java
@@ -0,0 +1,13 @@
+package org.javafling.pokerenlighter.simulation;
+
+/**
+ * Represents all the possible hand types a player can have.
+ * 
+ * @author Radu Murzea
+ * 
+ * @version 1.0
+ */
+public enum HandType
+{
+	RANDOM, RANGE, EXACTCARDS
+}
---
+++ src/org/javafling/pokerenlighter/simulation/PokerType.java
@@ -0,0 +1,13 @@
+package org.javafling.pokerenlighter.simulation;
+
+/**
+ * Represents all the different types of poker suppported by Poker Enlighter.
+ * 
+ * @author Radu Murzea
+ * 
+ * @version 1.0
+ */
+public enum PokerType
+{
+	TEXAS_HOLDEM, OMAHA, OMAHA_HILO
+}
