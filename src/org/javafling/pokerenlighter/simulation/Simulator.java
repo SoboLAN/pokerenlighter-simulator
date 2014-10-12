d10ad5d1f67c1d2361fe68da0d5a0733f3cd7706
Radu Murzea <radu.murzea@gmail.com>
2014-10-12 20:10:29 +0300

    The Simulator is now built using the Builder design pattern

--- src/org/javafling/pokerenlighter/simulation/Simulator.java
+++ src/org/javafling/pokerenlighter/simulation/Simulator.java
@@ -53,44 +53,224 @@ public final class Simulator implements WorkerNotifiable
     private SimulationNotifiable notifiable;
     
     private SimulationFinalResult simulationResult;
-
-    public Simulator(PokerType type, int rounds, SimulationNotifiable notifiable)
+    
+    public static class SimulatorBuilder
     {
-        if (type == null || rounds <= 0) {
-            throw new IllegalArgumentException("invalid arguments");
+        private PokerType gameType;
+        private ArrayList<PlayerProfile> profiles;
+        private int nrRounds;
+        private Card[] communityCards = new Card[5];
+        private int updateInterval;
+        private SimulationNotifiable notifiable;
+
+        public PokerType getGameType()
+        {
+            return this.gameType;
+        }
+
+        public SimulationNotifiable getNotifiable()
+        {
+            return this.notifiable;
+        }
+
+        public ArrayList<PlayerProfile> getProfiles()
+        {
+            return this.profiles;
+        }
+
+        public int getUpdateInterval()
+        {
+            return this.updateInterval;
+        }
+
+        public int getNrRounds()
+        {
+            return this.nrRounds;
+        }
+
+        public Card[] getCommunityCards()
+        {
+            return this.communityCards;
         }
         
-        this.gameType = type;
-        this.nrRounds = rounds;
-        this.profiles = new ArrayList<>();
-        this.communityCards = new Card[5];
+        public SimulatorBuilder setGameType(PokerType gameType)
+        {
+            this.gameType = gameType;
+            return this;
+        }
+        
+        public SimulatorBuilder addPlayer(PlayerProfile profile)
+        {
+            if (this.profiles == null) {
+                this.profiles = new ArrayList<>();
+            }
+            
+            this.profiles.add(profile);
+            
+            return this;
+        }
+
+        public SimulatorBuilder setFlop(Card[] flopCards)
+        {            
+            this.communityCards[0] = flopCards[0];
+            this.communityCards[1] = flopCards[1];
+            this.communityCards[2] = flopCards[2];
+            
+            return this;
+        }
+        
+        public SimulatorBuilder setTurn(Card turn)
+        {
+            this.communityCards[3] = turn;
+            return this;
+        }
+        
+        public SimulatorBuilder setRiver(Card river)
+        {
+            this.communityCards[4] = river;
+            return this;
+        }
+        
+        public SimulatorBuilder setNrRounds(int nrRounds)
+        {
+            this.nrRounds = nrRounds;
+            return this;
+        }
+
+        public SimulatorBuilder setUpdateInterval(int updateInterval)
+        {
+            this.updateInterval = updateInterval;
+            return this;
+        }
+
+        public SimulatorBuilder setNotifiable(SimulationNotifiable notifiable)
+        {
+            this.notifiable = notifiable;
+            return this;
+        }
+        
+        private boolean isPredictableResult()
+        {
+            boolean commSet = true;
+            for (int i = 0; i < 5; i++) {
+                if (this.communityCards[i] == null) {
+                    commSet = false;
+                    break;
+                }
+            }
+
+            if (commSet) {
+                boolean correctPlayerTypes = false;
+                for (PlayerProfile profile : this.profiles) {
+                    if (profile.getHandType() != HandType.EXACTCARDS) {
+                        correctPlayerTypes = true;
+                        break;
+                    }
+                }
+
+                if (! correctPlayerTypes) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        
+        private Card[] getAllCards()
+        {
+            ArrayList<Card> cards = new ArrayList<>();
+            
+            for (Card card : this.communityCards) {
+                if (card != null) {
+                    cards.add(card);
+                }
+            }
+            
+            for (PlayerProfile profile : this.profiles) {
+                if (profile.getHandType() == HandType.EXACTCARDS) {
+                    Card[] playerCards = profile.getCards();
+                    for (Card card : playerCards) {
+                        if (card != null) {
+                            cards.add(card);
+                        }
+                    }
+                }
+            }
+            
+            Card[] result = new Card[cards.size()];
+            
+            return cards.toArray(result);
+        }
+        
+        public Simulator build()
+        {
+            if (this.updateInterval <= 0 || 100 % this.updateInterval != 0) {
+                throw new IllegalStateException("Invalid update interval value");
+            } else if (this.gameType == null) {
+                throw new IllegalStateException("Invalid game type value");
+            } else if (this.nrRounds <= 0) {
+                throw new IllegalStateException("Invalid rounds value");
+            } else if (this.notifiable == null) {
+                throw new IllegalStateException("No notifiable was set");
+            } else if (this.profiles == null || this.profiles.size() < 2) {
+                throw new IllegalStateException("Invalid or insufficient player profiles");
+            }
+            
+            for (PlayerProfile profile : this.profiles) {
+                if (profile == null) {
+                    throw new NullPointerException("NULL PlayerProfile was used");
+                }
+            }
+            
+            //flop cards must either be all equal to NULL or all different from NULL
+            //if this is not the case (the "!" operator marks that), an exception is thrown
+            if (! (
+                (this.communityCards[0] == null && this.communityCards[1] == null && this.communityCards[2] == null)
+                ||
+                (this.communityCards[0] != null && this.communityCards[1] != null && this.communityCards[2] != null)
+                )) {
+                throw new IllegalStateException("Invalid flop cards");
+            }
+            
+            //if the turn is set, but not the flop, that's not OK
+            if (this.communityCards[3] != null && this.communityCards[0] == null) {
+                throw new IllegalStateException("Turn card can not exist without flop");
+            }
+            
+            //if the river is set, but not the turn, that's not OK
+            if (this.communityCards[4] != null && this.communityCards[3] == null) {
+                throw new IllegalStateException("River card can not exist without the turn card");
+            }
+            
+            if (this.isPredictableResult()) {
+                throw new IllegalStateException("The simulation result is predictable");
+            }
+            
+            Card[] allCards = this.getAllCards();
+            for (int i = 0; i < allCards.length - 1; i++) {
+                for (int j = i + 1; j < allCards.length; j++) {
+                    if (allCards[i].equals(allCards[j])) {
+                        throw new IllegalStateException("No duplicate cards allowed");
+                    }
+                }
+            }
+            
+            return new Simulator(this);
+        }
+    }
+
+    private Simulator(SimulatorBuilder builder)
+    {        
+        this.gameType = builder.getGameType();
+        this.nrRounds = builder.getNrRounds();
+        this.profiles = builder.getProfiles();
+        this.communityCards = builder.getCommunityCards();
+        this.updateInterval = builder.getUpdateInterval();
+        this.notifiable = builder.getNotifiable();
+        
         this.workers = new ArrayList<>();
-        this.updateInterval = 100;
         this.startTime = this.endTime = this.overallProgress = this.lastUpdatePercentage = 0;
         this.nrOfWorkers = SystemUtils.getNrOfLogicalCPUs();
-        this.notifiable = notifiable;
-    }
-        
-    /**
-     * Tells the Simulator how often to report progress. The progress will be reported by
-     * firing a property change event on all listening PropertyChangeListeners.
-     * <br />
-     * Note: Calling this method AFTER the Simulator started execution will have no effect. If
-     * this method is not called at all, then the progress will not be reported at all.
-     * 
-     * @param percentage the update interval. Value must be a strictly positive integer that divides 100.
-     * 
-     * @throws IllegalArgumentException if the parameter is an invalid value.
-     */
-    public void setUpdateInterval(int percentage)
-    {
-        if (percentage <= 0 || 100 % percentage != 0) {
-            throw new IllegalArgumentException("invalid update interval value");
-        }
-        
-        if (startTime == 0) {
-            updateInterval = percentage;
-        }
     }
     
     public boolean isRunning()
@@ -147,142 +327,12 @@ public final class Simulator implements WorkerNotifiable
         this.notifiable.onSimulationError(errorEvent);
     }
 
-    /**
-     * Adds a player to the simulation.
-     * <br />
-     * Note: if the profile has a set range of 100 %, it will be substituted with a new profile
-     * that has random as the hand type.
-     * 
-     * @param player the player.
-     * 
-     * @throws NullPointerException if player is null
-     * 
-     * @throws IllegalArgumentException if at least one of the cards contained within the player's
-     * profile are already contained within the previously added profiles or withing the community cards.
-     * This exception will be thrown only if the profile has a set hand type of PokerType.EXACTCARDS.
-     */
-    public void addPlayer(PlayerProfile player)
-    {
-        if (player == null) {
-            throw new NullPointerException("invalid player added");
-        }
-        
-        if (player.getHandType() == HandType.EXACTCARDS) {
-            Card[] newCards = player.getCards();
-            
-            for (Card newCard : newCards) {
-                if (isCardInProfiles(newCard) || isCardInCommunity(newCard)) {
-                    throw new IllegalArgumentException("card already exists");
-                }
-            }
-        }
-        
-        //if the player has a range of 100 % set, then it's a random hand
-        if (player.getHandType() == HandType.RANGE && player.getRange().getPercentage() == 100) {
-            this.profiles.add(new PlayerProfile(HandType.RANDOM, null, null));
-        } else {
-            this.profiles.add(player);
-        }
-    }
-    
-    /**
-     * Removes the player given as parameter.
-     * 
-     * @param player the PlayerProfile to be removed
-     * 
-     * @return true if the profile has been removed, false otherwise
-     */
-    public boolean removePlayer(PlayerProfile player)
-    {
-        if (player == null) {
-            return false;
-        }
-        
-        return this.profiles.remove(player);
-    }
-    
-    public void setFlop(Card[] flopCards)
-    {
-        if (flopCards == null) {
-            throw new NullPointerException("null cards not allowed");
-        } else if (flopCards.length != 3) {
-            throw new IllegalArgumentException("flop must have 3 cards");
-        } else if (flopCards[0] == null || flopCards[1] == null || flopCards[2] == null) {
-            throw new NullPointerException("null card not allowed");
-        }
-        
-        if (isCardInProfiles(flopCards[0]) || isCardInProfiles(flopCards[1]) || isCardInProfiles(flopCards[2])) {
-            throw new IllegalArgumentException("cards already exist");
-        }
-
-        //check if one of the new flop cards is not set as turn or river cards
-        if (flopCards[0].equals(communityCards[3])
-            || flopCards[0].equals(communityCards[4])
-            || flopCards[1].equals(communityCards[3])
-            || flopCards[1].equals(communityCards[4])
-            || flopCards[2].equals(communityCards[3])
-            || flopCards[2].equals(communityCards[4]))
-        {
-            throw new IllegalArgumentException("cards already exist");
-        }
-        
-        communityCards[0] = flopCards[0];
-        communityCards[1] = flopCards[1];
-        communityCards[2] = flopCards[2];
-    }
-    
-    /**
-     * Removes the flop cards.
-     */
-    public void removeFlop()
-    {
-        this.communityCards[0] = null;
-        this.communityCards[1] = null;
-        this.communityCards[2] = null;
-    }
-    
-    public void setTurn(Card turnCard)
-    {
-        if (turnCard == null) {
-            this.communityCards[3] = null;
-            return;
-        }
-        
-        if (isCardInProfiles(turnCard) || isCardInCommunity(turnCard)) {
-            throw new IllegalArgumentException("card already exists");
-        }
-        
-        this.communityCards[3] = turnCard;
-    }
-    
-    public void setRiver(Card riverCard)
-    {
-        if (riverCard == null) {
-            this.communityCards[4] = null;
-            return;
-        }
-        
-        if (isCardInProfiles(riverCard) || isCardInCommunity(riverCard)) {
-            throw new IllegalArgumentException("card already exists");
-        }
-        
-        this.communityCards[4] = riverCard;
-    }
-    
     public void start()
     {
         if (this.isRunning || this.simulationResult != null) {
             return;
         }
         
-        if (isPredictableResult()) {
-            throw new IllegalStateException("the simulation result is predictable");
-        }
-        
-        if (! isCorrectCommunitySequence()) {
-            throw new IllegalStateException("the community cards sequence is incorrect");
-        }
-
         this.latch = new CountDownLatch(this.nrOfWorkers);
         int roundsPerWorker = getNrOfRoundsPerWorker(this.nrOfWorkers);
         
@@ -350,93 +400,7 @@ public final class Simulator implements WorkerNotifiable
         
         return true;
     }
-    
-    private boolean isPredictableResult()
-    {
-        boolean commSet = true;
-        for (int i = 0; i < 5; i++) {
-            if (communityCards[i] == null) {
-                commSet = false;
-                break;
-            }
-        }
         
-        if (commSet) {
-            boolean correctPlayerTypes = false;
-            for (PlayerProfile profile : profiles) {
-                if (profile.getHandType() != HandType.EXACTCARDS) {
-                    correctPlayerTypes = true;
-                    break;
-                }
-            }
-            
-            if (! correctPlayerTypes) {
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    private boolean isCardInProfiles(Card card)
-    {
-        for (PlayerProfile profile : this.profiles) {
-            if (profile.getHandType() == HandType.EXACTCARDS) {
-                if (isCardInArray(card, profile.getCards())) {
-                    return true;
-                }
-            }
-        }
-        
-        return false;
-    }
-    
-    private boolean isCardInCommunity(Card card)
-    {
-        for (Card communityCard : this.communityCards) {
-            if (communityCard != null) {
-                if (card.equals(communityCard))
-                {
-                    return true;
-                }
-            }
-        }
-        
-        return false;
-    }
-    
-    private boolean isCardInArray(Card c, Card[] arr)
-    {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals(c)) {
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    private boolean isCorrectCommunitySequence()
-    {
-        //if turn is set
-        if (communityCards[3] != null) {
-            //if flop is not set
-            if (communityCards[0] == null || communityCards[1] == null || communityCards[2] == null) {
-                return false;
-            }
-        }
-        
-        //if river is set
-        if (communityCards[4] != null) {
-            //if turn is not set
-            if (communityCards[3] == null) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-    
     private int getNrOfRoundsPerWorker(int workers)
     {
         return (int) Math.ceil(1.0 * this.nrRounds / workers);
