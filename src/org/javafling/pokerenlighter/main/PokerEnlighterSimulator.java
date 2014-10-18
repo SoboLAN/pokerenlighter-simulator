0122149f16693a9f754e44e5f1844039cab9cfc3
Radu Murzea <radu.murzea@gmail.com>
2014-10-19 01:30:14 +0300

    Added main class with some basic information about the simulator

---
+++ src/org/javafling/pokerenlighter/main/PokerEnlighterSimulator.java
@@ -0,0 +1,21 @@
+package org.javafling.pokerenlighter.main;
+
+/**
+ *
+ * @author Radu Murzea
+ */
+public class PokerEnlighterSimulator
+{
+    public static final String VERSION = "2.4 build 562";
+    public static final String BUILD_DATE = "28 Sep 2014";
+    public static final String BUILD_JAVA_VERSION = "JDK 7 Update 51";
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Poker Enlighter " + VERSION);
+        System.out.println("Built on " + BUILD_DATE);
+        System.out.println("Built using " + BUILD_JAVA_VERSION);
+        System.out.println("Copyright (c) 2009 - 2014 Radu Murzea");
+        System.out.println("Licensed under LGPL v2.1");
+    }
+}
